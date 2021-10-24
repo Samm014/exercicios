5867747b609971eed0c5e4ab56b9054aab7f6394
Samm014 <samuel.medeiros014@gmail.com>
2021-10-24 08:23:24 -0300

    updateStyleContador

--- pages/estado/contador.jsx
+++ pages/estado/contador.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
+import styles from './css/styleContador.module.css';
 
 export default function contador(){
-
-    const [valor, setValor] = useState(5);
+    
+    const [valor, setValor] = useState(0);
 
     function getValor(e){
-       e.target.innerHTML === "Aumentar(+)" ? setValor(valor+1) : setValor(valor-1);
+        e.target.innerHTML === "Aumentar(+)" ? setValor(valor+1) : setValor(valor-1);
     }
 
     return (
-        <div>
-            <h1>Contador</h1>
-            <p>{valor}</p>
-            <button onClick={(e)=>getValor(e)}>Diminuir(-)</button>
-            <button onClick={(e)=>getValor(e)}>Aumentar(+)</button>
+        <div className={styles.body}>
+            <div className={styles.container}>
+                <h1 className={styles.title}>Contador</h1>
+                <p className={styles.valor}>{valor}</p>
+                <div className={styles.wrapButtons}>
+                    <button className={styles.button} onClick={(e)=>getValor(e)}>Diminuir(-)</button>
+                    <button className={styles.button} onClick={(e)=>getValor(e)}>Aumentar(+)</button>
+                </div>
+            </div>
         </div>
+        
     )
 } 
